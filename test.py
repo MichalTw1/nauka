46c69425010031eabdcc6a6ac00d8703272f7036
MichalTw1 <michaltw26@gmail.com>
2025-10-27 17:47:18 +0100

    Dodałem nowy plik testowy Python

---
+++ test.py
@@ -0,0 +1,6 @@
+#Suma dwóch liczb
+pierwsza_liczba = float(input("wpisz pierwszą liczbe: "))
+druga_liczba = float(input("wpisz drugą liczbe: "))
+
+suma = pierwsza_liczba + druga_liczba
+print(suma)
